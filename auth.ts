ab4178a3f3776e185f1b340287cf1feaf78caa9c
Sahalarbani <sahalpanglima@gmail.com>
2026-01-14 21:41:22 +0700

    fix  cache part2

--- auth.ts
+++ auth.ts
@@ -6,9 +6,22 @@ import { authConfig } from "./auth.config";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma) as any,
   session: { strategy: "database" },
-  trustHost: true, 
+  trustHost: true,
   ...authConfig,
+  // 👇 PAKSA COOKIE AGAR LENGKET (Brave Friendly)
+  cookies: {
+    sessionToken: {
+      name: `next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      },
+    },
+  },
   callbacks: {
+    ...authConfig.callbacks,
     async session({ session, user }: any) {
       if (session.user && user) {
         session.user.id = user.id;
