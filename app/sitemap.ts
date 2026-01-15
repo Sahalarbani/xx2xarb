ae4f854d5ab00aa8cf90d4af60f41e16377b966d
Sahalarbani <sahalpanglima@gmail.com>
2026-01-15 20:18:36 +0700

    fix deploy

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next';
 import { prisma } from '@/lib/prisma';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = 'https://arbskin.vercel.app'\;
+  const baseUrl = 'https://arbskin.vercel.app';
 
   const skins = await prisma.skin.findMany({
     select: { id: true, updatedAt: true },
